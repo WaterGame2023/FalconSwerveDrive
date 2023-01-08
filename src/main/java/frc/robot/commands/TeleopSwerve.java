015635f5f893e7e6ee4ceee9705777b1e9346715
WaterGame2023 <spam4alaric@gmail.com>
2023-01-08 14:11:10 -0600

    Deweird TeleopSwerve and delete velo auto

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class TeleopSwerve extends CommandBase {
 
+    //Variables that will be used in the command
     private double rotation;
     private Translation2d translation;
     private boolean fieldRelative;
@@ -20,13 +21,12 @@ public class TeleopSwerve extends CommandBase {
     private int strafeAxis;
     private int rotationAxis;
 
-    /**
-     * Driver control
-     */
-    public TeleopSwerve(Swerve swerveSubsystem, Joystick controller, int translationAxis, int strafeAxis, int rotationAxis, boolean fieldRelative, boolean openLoop) {
+    //Control Stuff for the driver
+    public TeleopSwerve(Swerve swerveSubsystem, Joystick controller, int translationAxis, int strafeAxis, int rotationAxis, boolean fieldRelative, boolean openLoop) { //Inputs from RobotContainer
         this.swerveSubsystem = swerveSubsystem;
         addRequirements(swerveSubsystem);
 
+        //Makes the variables defined earlier equal the values from RobotContainer
         this.controller = controller;
         this.translationAxis = translationAxis;
         this.strafeAxis = strafeAxis;
@@ -37,13 +37,13 @@ public class TeleopSwerve extends CommandBase {
 
     @Override
     public void execute() {
-        double yAxis = -controller.getRawAxis(translationAxis);
+        double yAxis = -controller.getRawAxis(translationAxis);  //Flips the controller values as needed and gets the value of each axis
         double xAxis = -controller.getRawAxis(strafeAxis);
         double rAxis = -controller.getRawAxis(rotationAxis);
         
-        // uwu what dis we are taking our constant deadbands from the constants file
-        // instead of entering them in some library or right here
-        yAxis = (Math.abs(yAxis) < Constants.stickDeadband) ? 0 : yAxis;
+        // Apply deadbands from constants file to each axis
+        yAxis = (Math.abs(yAxis) < Constants.stickDeadband) ? 0 : yAxis; //If the absolute value of the stick value is less than the deadband as defined in constants, the Axis=0
+        //Otherwise if the stick value is greater than the deadband, Axis = the stick value
         xAxis = (Math.abs(xAxis) < Constants.stickDeadband) ? 0 : xAxis;
         rAxis = (Math.abs(rAxis) < Constants.stickDeadband) ? 0 : rAxis;
 
