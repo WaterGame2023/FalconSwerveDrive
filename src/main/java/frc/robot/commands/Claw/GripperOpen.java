e36be4360c967e5b76335f912832af16404fdb76
A. Hanson <65613848+WaterGame2023@users.noreply.github.com>
2023-02-13 17:52:17 -0600

    Finish gripper open

--- src/main/java/frc/robot/commands/Claw/GripperOpen.java
+++ src/main/java/frc/robot/commands/Claw/GripperOpen.java
@@ -6,32 +6,50 @@ package frc.robot.commands.Claw;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.Constants.Swerve.Arm;
 import edu.wpi.first.math.controller.PIDController;
 import frc.robot.subsystems.*;
 import edu.wpi.first.math.controller.PIDController;
 import frc.robot.subsystems.*;
 
 public class GripperOpen extends CommandBase {
+
   private final ArmSubsystem m_armSubsystem;
+  private final PIDController gripperPIDController;
   
   public GripperOpen(ArmSubsystem m_armSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
       this.m_armSubsystem = m_armSubsystem;
       addRequirements(m_armSubsystem);
 
+      this.gripperPIDController = new PIDController(Arm.gripperKP, Arm.gripperKI, Arm.gripperKD);
+      gripperPIDController.setTolerance(1);
+      gripperPIDController.setSetpoint(Arm.gripperOpenPosition);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    System.out.println("Gripper opening!!!!");
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    double gripperSpeed = gripperPIDController.calculate(m_armSubsystem.getgripperPosition());
+
+    m_armSubsystem.setGripperSpeed(gripperSpeed);
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    System.out.println("Stopping the gripper!!!");
+    m_armSubsystem.stopGripper();
+  }
 
   // Returns true when the command should end.
   @Override
